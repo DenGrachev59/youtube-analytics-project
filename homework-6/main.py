4a8c059961364a788bb1028538039c89de86c7fa
DenGrachev59 <2717087@mail.ru>
2023-06-22 14:30:09 +0500

    решение 6й домашней работы

--- homework-6/main.py
+++ homework-6/main.py
@@ -2,5 +2,12 @@ from src.video import Video
 
 if __name__ == '__main__':
     broken_video = Video('broken_video_id')
+    broken_video1 = Video('AWX4JnAnjBE')
     assert broken_video.title is None
     assert broken_video.like_count is None
+
+    # print(broken_video1.title)
+    # print(broken_video.title)
+    # print(broken_video.like_count)
+    # print(broken_video.video_id)
+    # print(broken_video1.video_response['items'][0])
